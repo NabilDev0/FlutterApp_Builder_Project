cb27886f6137052c9d08c31a35ac10f625239380
NabilDiv0 <nabilmo.general@gmail.com>
2025-12-30 06:04:24 +0300

    fix:fixed swagger quick_generate api and changed the url

--- backend/urls.py
+++ backend/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('code_generator.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/',
+    path('api/swagger/',
          SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
